1dacf0648850ca6246a416a71f01b44730e2f71b
Felipe Schulz <fschulzfurtado@gmail.com>
2025-05-18 22:17:19 +0100

    redacted commit

--- apps/api/src/Shared/TwiML/Schemas/IncomingCall.ts
+++ apps/api/src/Shared/TwiML/Schemas/IncomingCall.ts
@@ -19,7 +19,7 @@ import { z } from 'zod';
  *   CallerZip: 'no value',
  *   CallSid: 'CA07c4e00df86d68fac9e58479ce17c532',
  *   CallStatus: 'ringing',
- *   CallToken: '%7B%22parentCallInfoToken%22%3A%22eyJhbGciOiJFUzI1NiJ9.eyJjYWxsU2lkIjoiQ0EwN2M0ZTAwZGY4NmQ2OGZhYzllNTg0NzljZTE3YzUzMiIsImZyb20iOiIrMzUxOTExNzQwNjcxIiwidG8iOiIrMTg2NDUzMTg4OTMiLCJpYXQiOiIxNzQ3NDg1NzkzIn0.FrvMbsDS3fAoKfuzzEm_ZouMqnJqGWbfwtv90QJgpnbZh3Vg4q_a9iHAHedrxRoisQwiVslmiExpgfvpLbBkRg%22%2C%22identityHeaderTokens%22%3A%5B%5D%7D',
+ *   CallToken: 'REDACTED',
  *   Direction: 'inbound',
  *   From: '+351911740671',
  *   FromCity: 'no value',
